2f681274d49b138b8a3f8bebcc49ae31d2ebc9f8
Cantikaamelia <cantikaamelia282@gmail.com>
2025-10-05 21:38:44 +0700

    done pak

---
+++ index.js
@@ -0,0 +1,25 @@
+const tambah = (a,b) => {
+    return a + b;
+}
+console.log(tambah(10,15));
+
+const kurang = (a,b) => {
+    return a - b;
+}
+console.log(kurang(40,10));
+
+const kali = (a,b) => {
+    return a * b;
+}
+console.log(tambah(2,5));
+
+const bagi = (a,b) => {
+    return a / b;
+}
+console.log(bagi(2,20));
+
+const sisabagi = (a,b) => {
+    return a % b;
+}
+console.log(sisabagi(10,5));
+
